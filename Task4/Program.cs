aaaab52ecc75f690b3560cb3b82c6f5d2272c635
Zhmaeva Marina <lady.crupsckaya2012@yandex.ru>
2022-11-02 20:19:59 +0500

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+
+
+while(startNumber <= number)
+    {
+        if(startNumber % 2 ==0)
+        Console.Write(startNumber + ", ");
+        startNumber ++;
+    }
